2a4e086eca756d538c62f35cb3171cf1aab3813c
massao000 <onepiece.4224y@icloud.com>
2022-11-10 03:18:50 +0900

    ライブラリ

---
+++ app/controller/conIndex.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once __DIR__ . "/../views/vieIndex.php";
---
+++ app/views/vieIndex.php
@@ -0,0 +1,10 @@
+<?php require_once __DIR__ . "/../template/base1.html"; ?>
+<!-- 各ページで使うcssやjsをここに配置 -->
+<?php require_once __DIR__ . "/../template/base1_close_body_start.html"; ?>
+<!-- ここにメインコンテンツ -->
+
+<main>
+    <h1>テスト</h1>
+</main>
+
+<?php require_once __DIR__ . "/../template/base2_body_close.html"; ?>
